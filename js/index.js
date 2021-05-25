bf9d6a3c48086e3126634d9c13b9ae99244342db
Dmitriy <dmitriypruss21041984@gmail.com>
2021-05-25 23:57:21 +0300

    4 commit

--- js/index.js
+++ js/index.js
@@ -2,101 +2,104 @@
 
 // 1. Реализовать возведение в целую степень. (pow(base, exponent) => base**exponent, 
 // где base любое число, exponent - натуральное (1, 2, 3) или * целое число(-2, -1, 0, 1, 2)).
-
-const pow = (num, n) => {
-    switch (true) {
-        case (n === 0):
-            return 1;
-            break;
-        case (n >= 1):
-            return num * pow(num, n-1);
-            break;
-        case (n < 0):
-            return 1 / num * pow(num, n + 1);
-            break;
-        default:
-            return 'It is not an integer number';
-            break; 
+console.groupCollapsed('Task 1');
+    const pow = (num, n) => {
+        switch (true) {
+            case (n === 0):
+                return 1;
+                break;
+            case (n >= 1):
+                return num * pow(num, n-1);
+                break;
+            case (n < 0):
+                return 1 / num * pow(num, n + 1);
+                break;
+            default:
+                return 'It is not an integer number';
+                break; 
+        };
     };
-};
 
-console.log('pow(5, 3) :>> ', pow(5, 3));
-console.log('pow(5, 0) :>> ', pow(5, 0));
-console.log('pow(2, -3) :>> ', pow(2, -3));
-console.log('pow(5, "asd") :>> ', pow(5, 'asd'));
+    console.log('pow(5, 3) :>> ', pow(5, 3));
+    console.log('pow(5, 0) :>> ', pow(5, 0));
+    console.log('pow(2, -3) :>> ', pow(2, -3));
+    console.log('pow(5, "asd") :>> ', pow(5, 'asd'));
+console.groupEnd();
+
 
 // 2. Реализовать вывод в консоль скобок (
 // bracketWrapper3(3); // => сразу в консоль)
+console.groupCollapsed('Task 2');
+    bracketWrapper3(5);
 
-bracketWrapper3(5);
-
-function bracketWrapper3(n) {
-    const brackets = [];
-    bracketWrapperRec(n);
-    
-    function bracketWrapperRec(n) {
-        if (n === 0) {          
-            return;
+    function bracketWrapper3(n) {
+        const brackets = [];
+        bracketWrapperRec(n);
+        
+        function bracketWrapperRec(n) {
+            if (n === 0) {          
+                return;
+            };
+            brackets.push('(');
+            bracketWrapperRec(n - 1);
+            brackets.push(')');
         };
-        brackets.push('(');
-        bracketWrapperRec(n - 1);
-        brackets.push(')');
+        console.log('brackets.join("") :>> ', brackets.join(''));
+        return brackets;
     };
-    console.log('brackets.join("") :>> ', brackets.join(''));
-    return brackets;
-};
+console.groupEnd();
 
 
 // 3. ** Реализовать функцию аналог flat для массивов. 
 // (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/flat)
 
-console.group('Task 12 - method flat');
+console.group('** Task 3 - method flat');
     function MyArray() {
         if (!new.target) {
-          return new MyArray();
-        };
+            return new MyArray();
+        }
         this.length = 0;
-    };
-      
-    const myArrayProto = new MyArray();
-
-
+    }
     
-
-    myArrayProto.pop = function() {
-        if(this.length === 0) {
+    const myArrayProto = new MyArray();
+    
+    myArrayProto.pop = function () {
+        if (this.length === 0) {
             return;
-        };
-        const lastItem = this[this.length-1]
+        }
+        const lastItem = this[this.length - 1];
         delete this[--this.length];
         return lastItem;
     };
-    myArrayProto.push = function(item) {
+    
+    myArrayProto.push = function (item) {
         this[this.length] = item;
         return ++this.length;
     };
-    myArrayProto.shift = function() {
-        if ( this.length === 0 ) {
-          return;
+    
+    myArrayProto.shift = function () {
+        if (this.length === 0) {
+            return;
         }
         const fistItem = this[0];
-        for(let i = 0; i < this.length - 1; i++) {
-          this[i] = this[i+1];
+        for (let i = 0; i < this.length - 1; i++) {
+            this[i] = this[i + 1];
         }
         delete this[--this.length];
         return fistItem;
     };
-    myArrayProto.unshift = function(item) {
-        for(let i = this.length; i >= 0; i--) {
-            this[i] = this[i - 1];
-        };
-        this[0] = item;
-        return ++this.length;
+    
+    myArrayProto.filter = function (callback) {
+        const returningArray = new MyArray();
+        for (let i = 0; i < this.length; i++) {
+            if (callback(this[i], i, this)) {
+                returningArray.push(this[i]);
+            }
+        }
+        return returningArray;
     };
-
-
-    MyArray.prototype = myArrayProto;
-        
+  
+    // flat method
     myArrayProto.flat = function(n) {
 
         const superObj = new MyArray();
@@ -120,7 +123,7 @@ console.group('Task 12 - method flat');
                         }; 
                     } else {
                         this[i].forEach(item => {
-                            if (!Array.isArray(item) ) {
+                            if ( !Array.isArray(item) ) {
                                 superObj.push(item);
                             } else if (n === 1) { 
                                 superObj.push(item);
@@ -128,7 +131,7 @@ console.group('Task 12 - method flat');
                                 flatten.call(this[i], n - 1);
                             }
                         });
-                    }
+                    };
                 } else if (this[i] === ' ') {
                     continue;
                 } else {
@@ -141,9 +144,9 @@ console.group('Task 12 - method flat');
         return superObj;
     };
 
-
+    MyArray.prototype = myArrayProto;
+  
     const myArr1 = new MyArray();
-
 
     for(let i = 0; i <= 6; i++) {
         if (i === 2) {
@@ -161,8 +164,7 @@ console.group('Task 12 - method flat');
 
         myArr1.push(i);
     };
-    console.log(myArr1);
-
+    console.log(`myArr1 >>`, myArr1);
     
     const newArr1 = myArr1.flat();
     console.log('newArr1 :>> ', newArr1);
@@ -175,5 +177,4 @@ console.group('Task 12 - method flat');
 
     const newArr4 = myArr1.flat(0);
     console.log('newArr4 :>> ', newArr4);
-
 console.groupEnd();
